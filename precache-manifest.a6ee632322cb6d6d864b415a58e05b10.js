3a66b2b3f5bfdf64cef3da2136ad091c772f8a0c
Honey Soni <honeysoniverse@gmail.com>
2022-01-13 10:31:12 +0530

    Updates

---
+++ precache-manifest.a6ee632322cb6d6d864b415a58e05b10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5aae14a56486a31a0bd2694a9dc770",
+    "url": "/Intelliatech-Employee-Portal/index.html"
+  },
+  {
+    "revision": "ec2553d964df2990a768",
+    "url": "/Intelliatech-Employee-Portal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "56b469c9706c74a6411f",
+    "url": "/Intelliatech-Employee-Portal/static/js/2.52c095c2.chunk.js"
+  },
+  {
+    "revision": "9a004585169dcf43f5e0",
+    "url": "/Intelliatech-Employee-Portal/static/js/3.6f05c816.chunk.js"
+  },
+  {
+    "revision": "ec2553d964df2990a768",
+    "url": "/Intelliatech-Employee-Portal/static/js/main.5aa064cd.chunk.js"
+  },
+  {
+    "revision": "54869e7eb1c9eb4915bb",
+    "url": "/Intelliatech-Employee-Portal/static/js/runtime~main.bef2fea3.js"
+  },
+  {
+    "revision": "a2a60676b32f9e639892a9d65b7eff8e",
+    "url": "/Intelliatech-Employee-Portal/static/media/loading.a2a60676.gif"
+  }
+]);
